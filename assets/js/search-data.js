3f1b88f5a59dc31f4bba4691cc9046bdd31f97e5
YIFANK <84063730+YIFANK@users.noreply.github.com>
2026-03-09 20:48:31 +0000

    Deploying to gh-pages from @ YIFANK/yifank.github.io@470b30d8e8f06231869219dacc7b16304607f652 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,6 +110,13 @@ ninja.data = [{
           window.open("https://github.com/yifank", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yifan-kang-etk", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
